15f2bd42ce37414971b88729d1bc1ab901c97a34
林孝勳 <113780009+DannyXunLin@users.noreply.github.com>
2026-05-06 18:13:12 +0800

    Update container image and pull policy

--- algorithms/warm-pool.groovy
+++ algorithms/warm-pool.groovy
@@ -62,8 +62,8 @@ kind: Pod
 spec:
   containers:
   - name: defects4j
-    image: defects4j-lang-bug1:latest
-    imagePullPolicy: IfNotPresent
+    image: 192.168.56.10:5000/defects4j-lang-bug1:latest
+    imagePullPolicy: Always
     tty: true
     command: [cat]
     resources:
