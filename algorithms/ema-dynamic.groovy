a9f627d4c6203f561a776379d9740efead98a929
林孝勳 <113780009+DannyXunLin@users.noreply.github.com>
2026-05-06 18:15:22 +0800

    Update Docker image and pull policy for defects4j

--- algorithms/ema-dynamic.groovy
+++ algorithms/ema-dynamic.groovy
@@ -69,8 +69,8 @@ kind: Pod
 spec:
   containers:
   - name: defects4j
-    image: defects4j-${task.bug}:latest
-    imagePullPolicy: IfNotPresent
+    image: 192.168.56.10:5000/defects4j-lang-bug1:latest
+    imagePullPolicy: Always
     tty: true
     command: [cat]
     resources:
