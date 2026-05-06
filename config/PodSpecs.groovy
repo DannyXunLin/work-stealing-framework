e0c1d0f7dd770028d07e56def55529963e0023f7
林孝勳 <113780009+DannyXunLin@users.noreply.github.com>
2026-05-07 01:11:05 +0800

    Update resource requests and JVM options in PodSpecs

--- config/PodSpecs.groovy
+++ config/PodSpecs.groovy
@@ -1,7 +1,7 @@
 def getResources() {
-    return [requests: [cpu: "1", memory: "2Gi"], limits: [cpu: "2", memory: "4Gi"]]
+    return [requests: [cpu: "500m", memory: "512Mi"], limits: [cpu: "1", memory: "1Gi"]]
 }
 def getJvmOpts() {
-    return "-Xmx2g -XX:MaxPermSize=512m"
+    return "-Xmx512m -XX:MaxPermSize=256m"
 }
 return this
