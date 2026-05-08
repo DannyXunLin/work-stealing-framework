e1a86c4696862f6dfb40732ce41ecbc36a6af706
林孝勳 <113780009+DannyXunLin@users.noreply.github.com>
2026-05-08 15:35:39 +0800

    Update chunkResultFile path in work-stealing script

--- algorithms/work-stealing-no-chunk.groovy
+++ algorithms/work-stealing-no-chunk.groovy
@@ -64,7 +64,7 @@ spec:
 """) {
                     node(podLabel) {
                         container('defects4j') {
-                            def chunkResultFile = "chunk_result_${BUILD_ID}_${currentWorkerId}_${task.id.replace('-', '_')}.txt"
+                            def chunkResultFile = "/home/jenkins/agent/workspace/work-stealing-comparison/chunk_result_${BUILD_ID}_${currentWorkerId}_${task.id.replace('-', '_')}.txt"
                             def shellScript = """cd /workspace
 export ANT_OPTS='${jvmOpts}'
 start=\$(date +%s%3N)
